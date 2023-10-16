2f92df55d6cec21c723615510f1df459f72356be
Jose DGiovanni <josedgiovanni9@gmail.com>
2023-10-16 00:14:05 -0400

    prueba de cambio de color de texto

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, 1);
+    
+    cout << "Hola mundo" << endl;
+    
+    SetConsoleTextAttribute(hConsole, 2);
+    
+    cout << "Adios mundo" << endl;
+    
+    SetConsoleTextAttribute(hConsole, 0);
+    
+    return 0;
+}
